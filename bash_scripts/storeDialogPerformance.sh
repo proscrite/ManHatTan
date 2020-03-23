80f66e09297d16e611e15aa19c727d9ef9aeb333
proscrite <pablogfr94@gmail.com>
2020-03-23 10:52:00 +0100

    Bash dialog and performance manager (rnd and choice exercise intervals)

---
+++ bash_scripts/storeDialogPerformance.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+dialog="/Users/pabloherrero/Documents/ManHatTan/applescripts/usingBundle.scpt"
+pyupdate="/Users/pabloherrero/Documents/ManHatTan/python_scripts/updatePerformance.py"
+selGota="/Users/pabloherrero/Documents/ManHatTan/applescripts/selectGota.scpt"
+#gota="/Users/pabloherrero/Documents/ManHatTan/GOTAs/Die_Verwandlung.got"
+gota="$(osascript $selGota 2>^1)"
+echo "Practicing vocabulary from book $gota"
+
+min=$((24 * 60))
+rmin=$(( $RANDOM % $min ))
+#at -f "$dialog" now+${rmin}min
+rmin=$1
+CONTINUE=true
+while $CONTINUE; do
+
+  for ((c=$rmin; c>=1; c--))
+  do
+      sleep 1
+    done
+
+  OUTPUT="$(osascript $dialog $gota)"
+
+  if [[  $OUTPUT == *"User cancelled"*  ]];
+  then
+    echo "User cancelled: exiting program"
+    break
+  else
+      echo "Output from dialog: $OUTPUT"
+      python $pyupdate $gota $OUTPUT
+  fi
+
+
+done
