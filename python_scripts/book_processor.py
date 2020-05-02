13a9b7c92ea01ef0b4c0d3173d81ea4186967758
proscrite <pablogfr94@gmail.com>
2020-05-02 19:42:10 +0200

    Added GOogle Saved Translations (GOST) import option

--- python_scripts/book_processor.py
+++ python_scripts/book_processor.py
@@ -11,13 +11,12 @@ sys.path.append('~/Documents/ManHatTan/python_scripts/')
 from rashib import *
 from krahtos import *
 from bulkTranslate import *
+from gost import *
 from init_lipstick import *
 
 ######### Main #########
-if __name__ == "__main__":
-    filename = f'/Users/pabloherrero/Documents/ManHatTan/kindle_raw/Il castello dei destini incrociati - Bloc-notes.html'
-    dest_lang = 'es'
-    src_lang = 'it'
+def book_processor_main(filename: str, dest_lang: str, src_lang: str):
+
     filepath, basename = os.path.split(filename)
 
     if 'html' in basename:
@@ -27,6 +26,11 @@ if __name__ == "__main__":
     elif '.docx' in basename:
         print('Google Books file detected')
         cder_path = rashib_main(filename)
+    elif '.csv' in basename:
+        print('Google Saved Translations detected')
+        lippath = gost_main(filename, dest_lang, src_lang)
+        print("Done! Now you can start practicing")
+        return 0
 
     assert '.cder' in cder_path, "Wrong CADERA extension"
     print("Starting automatic translation...")
@@ -36,6 +40,13 @@ if __name__ == "__main__":
     print('Initializing word bank...')
     lippath = init_lipstick_main(gota_path)
     print("Done! You can start practicing")
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    dest_lang = sys.argv[2]
+    src_lang = sys.argv[3]
+    book_processor_main(filename, dest_lang, src_lang)
+
 #...
 
 # GUI select raw kindle/playbooks -> file
