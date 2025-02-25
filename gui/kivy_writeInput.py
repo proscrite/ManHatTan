195f1fd1a0cbb3e637b8ff17e2b3622e045e14b3
proscrite <pablogfr94@gmail.com>
2025-02-25 11:03:22 +0200

    plot_combat_stats in writeInput, update for animation

--- gui/kivy_writeInput.py
+++ gui/kivy_writeInput.py
@@ -8,11 +8,30 @@ from kivy.uix.popup import Popup
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
-
 from kivy.core.window import Window
+from kivy.clock import Clock
+
+from add_correctButton import CorrectionDialog
+from gui.kivy_multipleAnswer import set_question, update_all
+from formats.format_text_input import FTextInput, RTLTextInput
+from gui.plot_pkmn_panel import *
+
+import threading
+import time
+import matplotlib.pyplot as plt
+import matplotlib.transforms as mtransforms
+import matplotlib.patches as mpatch
+from matplotlib.gridspec import GridSpec
+from matplotlib.patches import FancyBboxPatch
+
+from io import BytesIO
+from skimage.io import imread
+from skimage import color, img_as_ubyte
+from skimage.transform import resize
+
+from time import sleep
 from bidi.algorithm import get_display
 
-from kivy.clock import Clock
 
 from functools import partial
 from time import sleep
@@ -29,9 +48,7 @@ sys.path.append(ROOT_PATH+'/scripts/ML_duolingo')
 
 from update_lipstick import *
 from duolingo_hlr import *
-from add_correctButton import CorrectionDialog
-from kivy_multipleAnswer import set_question, update_all
-from formats.format_text_input import FTextInput, RTLTextInput
+
 
 def strip_accents(s):
     print('Input string: ', s)
@@ -42,31 +59,39 @@ def strip_accents(s):
 
 class WriteInput(App):
 
-    def __init__(self, lipstick: pd.DataFrame, lippath: str,
-      modality: str, word_ll: str, word_ul: str):
+    def __init__(self, lippath: str, modality: str):
         App.__init__(self)
-        self.lipstick = lipstick
         self.lippath = lippath
         self.modality = modality
-        self.word_ll: str = word_ll
-        self.word_ul: str = word_ul
-        self.rtl_flag = False
-        if self.lipstick.learning_language.iloc[0] == 'iw':
-            self.rtl_flag = True
 
-        self.grid = GridLayout(cols=2, padding=80, spacing=40)
+        self.lipstick = self.load_lipstick()
+        self.rtl_flag = (self.lipstick.learning_language.iloc[0] == 'iw')
 
-    def load_question(self):
-        if self.modality == 'dt':
+        self.word_ll, self.word_ul, self.iqu, self.nid = set_question(self.lippath, self.rtl_flag, size_head=6)
+        if self.modality == 'dt': 
             self.question, self.answer = self.word_ll, self.word_ul
             self.checkEntry = 'word_ul'
-
         elif self.modality == 'rt':
             self.question, self.answer = self.word_ul, self.word_ll
             self.checkEntry = 'word_ll'
 
         else:
             print('Error: modality is not "dt" or "rt"')
+
+        self.box = BoxLayout(orientation = 'horizontal') # Used this to nest grid into box
+        self.InputPanel = GridLayout(cols= 1, rows=2, size_hint_x=0.8, size_hint_y = 1, padding=20, spacing=20)
+        self.optMenu = GridLayout(cols=1, rows=2, size_hint_x=0.2, size_hint_y=0.5, padding=20, spacing=20)
+        # self.grid = GridLayout(cols=2, padding=80, spacing=40)
+    
+    def load_lipstick(self):
+        if self.modality == 'rt': self.index = 'word_ll'
+        elif self.modality == 'dt': self.index = 'word_ul'
+
+        lipstick = pd.read_csv(self.lippath)
+        lipstick.set_index(self.index, inplace=True, drop=False)
+        return lipstick
+
+    def load_question(self):
 
         if self.rtl_flag: 
             self.question_displ = get_display(self.question)
@@ -76,16 +101,13 @@ class WriteInput(App):
             self.question_displ = self.question
             self.answer_displ = self.answer
 
-        print(f'self.rtl_flag = {self.rtl_flag}')
-        print(f'self.question = {self.question}, self.question_displ = {self.question_displ}')
-        lb = Label(text='Translate: %s'%self.question_displ, font_size=40, font_name = FONT_HEB, size_hint=(2,1), )
-        self.giveup = Button(text='Exit', on_release=self.exit, size_hint=(0.5,1), font_size=40,
-            background_color=(0.6, 0.5, 0.5, 1))
-        self.grid.add_widget(lb)
-        self.grid.add_widget(self.giveup)
-
+        # lb = Label(text='Translate: %s'%self.question_displ, font_size=40, font_name = FONT_HEB, size_hint=(2,1), )
+        # self.InputPanel.add_widget(lb)
+        
+    def load_input(self):
+        
         input_callback = partial(self.checkAnswer)
-        self.text_input_focus = False
+        self.text_input_focus = True
         #self.input = TextInput(hint_text='Write here', multiline=False,
          # on_text_validate=input_callback, focus=self.text_input_focus, size_hint=(2,1)) # Add hint button?
         
@@ -99,15 +121,21 @@ class WriteInput(App):
                                     pos_hint={"center_x": 0.5, "center_y": 0.5}, center_y=0.5, halign="center",)
         
 
+        self.InputPanel.add_widget(self.input)
+        
         enter = Button(text='Enter', on_release=input_callback, size_hint=(0.5,1))
-        self.grid.add_widget(self.input)
-        self.grid.add_widget(enter)
+        self.optMenu.add_widget(enter)
+   
+    def load_options(self):
+        self.giveup = Button(text='Exit', on_release=self.exit, size_hint=(0.5,1), font_size=40,
+            background_color=(0.6, 0.5, 0.5, 1))
+        self.optMenu.add_widget(self.giveup)
 
     def checkAnswer(self, instance):
-
-        print('Checking answer: ', self.input.text)
+        elapsed_time = time.time() - self.start_time
+        self.speed = 1/elapsed_time
+        
         checkPop = GridLayout(cols=2, padding=10)
-        print(self.question_displ)
         lb2 = Label(text='Translate: %s'%self.question_displ, font_name=FONT_HEB, font_size = 40, size_hint=(2,1), )
         checkPop.add_widget(lb2) # Keep same label as in previous screen
         """TEST:
@@ -119,26 +147,20 @@ class WriteInput(App):
         if self.rtl_flag: 
             self.input.text_displ = get_display(self.input.text)
             input_answer = self.input.text_displ
-            correct_answer = self.lipstick.loc[self.word_ul, self.checkEntry]
+
         else:
             self.input.text_displ = self.input.text
             input_answer = strip_accents(self.input.text.lower())
-            correct_answer = strip_accents((self.lipstick.loc[self.word_ul, self.checkEntry]).lower() )
-        print('self.lipstick.loc[self.word_ul, self.checkEntry] = ', self.lipstick.loc[self.word_ul, self.checkEntry])
-        print('equality condition: ', self.lipstick.loc[self.word_ul, self.checkEntry] == self.input.text_displ )
-        #self.lipstick.set_index('word_ll', inplace=True, drop=False)
+            self.answer = strip_accents(self.answer.lower())
 
+        if self.answer == input_answer:
 
-        if correct_answer == input_answer:
-            print('Correct answer')
             yes = Button(text='Correct! The translation is \n'+self.answer_displ, font_size=40, font_name=FONT_HEB, size_hint=(2,1),
               background_color=(0, 1, 0, 1))
             checkPop.add_widget(yes)
             self.perf = 1
-            print('ENTERED CORRECT ANSWER CONDITION')
 
         # elif self.double_check_input(correct_answer, input_answer)[0]:
-        #     print('Correct answer')
         #     baseAnsw = self.double_check_input(correct_answer, input_answer)[1]
         #     yes = Button(text='Correct! The baseform translation is \n'+baseAnsw, font_name=FONT_HEB, font_size=40, size_hint=(2,1),
         #       background_color=(0, 1, 0, 1))
@@ -146,7 +168,6 @@ class WriteInput(App):
         #     self.perf = 1
 
         else:
-            print('Incorrect answer')
             no = Button(text=self.input.text+': Incorrect! The translation is \n'+self.answer_displ, font_name=FONT_HEB, font_size=40, size_hint=(2,1),
               background_color=(1, 0, 0, 1))
             checkPop.add_widget(no)
@@ -159,7 +180,38 @@ class WriteInput(App):
         popChecker.open()
         Window.bind(on_key_down=self._on_keyboard_handler)
 
+    def plot_combat_stats(self, entry_stats, ax = None):
+        
+        positions = [(0.125, 0.75), (0.25, 0.75), (0.375, 0.75), (0.175, 0.4), (0.325, 0.4)]
+        fig = plt.figure(figsize=(12, 4))
+        fig.patch.set_facecolor("black")
+        gs = GridSpec(3, 4, width_ratios=[0.3, 0.3, 0.3, 0.9], height_ratios=[1, 1, 1])
 
+        colors = ['gold', 'maroon', 'magenta', 'navy', 'darkorange']
+        
+        for i, ((x, y), (k, v) )in enumerate(zip(positions, entry_stats.items()) ):
+            ax = fig.add_axes([x - 0.1, y - 0.1, 0.3, 0.3])  # [x, y, width, height]
+            ax.pie([v, 10-v], wedgeprops=dict(width=0.5), startangle=270, colors=[colors[i], '0.9'])
+            ax.set_xlabel(k, fontsize=16)
+            ax.xaxis.label.set_color('yellow')
+
+        health_ax = fig.add_subplot(gs[2, :3])  # Spans the second row, columns 2-3
+        health_ax.set_xlim(0, 1)
+        # health_ax.set_ylim(0, 1)
+        draw_health_bar(entry_stats, health_ax)
+
+        axim = fig.add_subplot(gs[:, 3])         # Spans all rows, last column
+        impath = PATH_ANIM+str(self.nid).zfill(3)+'.png'
+        self.anim = imread(impath)
+        
+        img = self.anim[:, self.anim.shape[0] * self.nframe: self.anim.shape[0] * (self.nframe+1) , :]        
+        self.img_display = axim.imshow(img)  # Store reference to imshow
+        axim.set(xlabel=f'Translate: {self.question_displ}',)
+        axim.xaxis.label.set_color('yellow')
+        axim.xaxis.label.set_fontsize(26)
+
+        return fig
+    
     ##### Attempt of keyboard_handler #####
     def _on_keyboard_handler(self, instance, keyboard, keycode, *args):
         if keycode == 40:
@@ -189,7 +241,8 @@ class WriteInput(App):
 
     def on_close(self, *args):
         print('Mode: ', 'w'+self.modality)
-        update_all(self.lipstick, self.lippath, self.word_ul, self.perf, mode='w'+self.modality)
+        update_all(self.lipstick, self.lippath, self.word_ul, 
+                   self.perf, self.speed, mode='w'+self.modality)
         App.stop(self)
 
     def exit(self, instance):
@@ -197,22 +250,39 @@ class WriteInput(App):
         App.stop(self)
 
     def build(self):
-        return self.grid
+        self.load_question()
+
+        self.nframe = 0
+        self.lipstick.set_index('n_id', inplace=True, drop=False)
+
+        entry_stats = load_pkmn_stats(self.lipstick, self.nid)
+        self.fig = self.plot_combat_stats(entry_stats)
+        self.canvas = FigureCanvasKivyAgg(self.fig)
+
+        self.InputPanel.add_widget(self.canvas)
+        # self.lipstick.set_index(self.checkEntry, inplace=True, drop=False)
+
+        self.load_input()
+        self.load_options()        
+        Clock.schedule_interval(self.update, 1 / 30)  # 30 FPS
+        self.box.add_widget(self.InputPanel)
+        self.box.add_widget(self.optMenu)
+        return self.box
+
+    def update(self, dt):
+        """Update function for animation"""
+        self.nframe = (self.nframe + 1) % (self.anim.shape[1] // self.anim.shape[0])  # Loop through frames
+
+        new_img = self.anim[:, self.anim.shape[0] * self.nframe: self.anim.shape[0] * (self.nframe+1), :]
+        self.img_display.set_data(new_img)  # Update image data
+
+        self.canvas.draw()  # Redraw the canvas
 
 if __name__ == "__main__":
-    lipstick_path = sys.argv[1]
-    #lipstick_path = '/Users/pabloherrero/Documents/ManHatTan/LIPSTICK/Die_Verwandlung.lip'
-    lipstick = pd.read_csv(lipstick_path)
-    lipstick.set_index('word_ul', inplace=True, drop=False)
-
-    word_ll, word_ul, iqu = set_question(lipstick_path, size_head=10)
-    #print(lipstick.loc[qu])
-
-    WI = WriteInput(lipstick, lipstick_path,
-      word_ll=word_ll, word_ul=word_ul, modality='dt')
-    WI.load_question()
-    #WI.load_options(qu, answ)  # Only exit button, correctionDialog after checking
-    #WI.load_answers(shufOpts)
+    # lipstick_path = sys.argv[1]
+    lipstick_path = '/Users/pabloherrero/Documents/ManHatTan/data/processed/LIPSTICK/hebrew_db.lip'
+    
+    WI = WriteInput(lipstick_path, modality='dt')
 
     perf = WI.run()
     print(perf)
