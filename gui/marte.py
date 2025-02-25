0517da7417e7382aaf97d1e7ea59de668eae7799
proscrite <pablogfr94@gmail.com>
2025-02-25 11:15:50 +0200

    updated madte/marte for compatibility with new NA version

--- gui/marte.py
+++ gui/marte.py
@@ -1,30 +1,32 @@
 ### MARTE: Multiple Answer Reverse Translation Exercise ###
 from time import sleep
 import sys
-sys.path.append('../python_scripts/')
-sys.path.append('../ML')
+ROOT_PATH = '/Users/pabloherrero/Documents/ManHatTan/'
+sys.path.append(ROOT_PATH+'/scripts/python_scripts/')
+sys.path.append(ROOT_PATH+'/scripts/ML_duolingo')
 
-from update_lipstick import *
 from duolingo_hlr import *
-from kivy_multipleAnswer import *
+from update_lipstick import *
+# from kivy_multipleAnswer import *
+from gui.kivy_multipleAnswer import *
 
-if __name__ == "__main__":
-    lipstick_path = sys.argv[1]
-    #lipstick_path = '/Users/pabloherrero/Documents/ManHatTan/LIPSTICK/Die_Verwandlung.lip'
-    lipstick = pd.read_csv(lipstick_path)
-    lipstick.set_index('word_ll', inplace=True, drop=False)
+import rnd_exercise_scheduler as daemon
 
-    answ, qu, iqu = set_question(lipstick_path, size_head=10)
-    #print(lipstick.loc[qu])
+def marte_main(lipstick_path, *largs):
+    print('Welcome to MARTE: Multioption Answer Reverse Translation Exercise')
+    if lipstick_path is None:
+        if len(sys.argv) > 1:
+            lipstick_path = sys.argv[1]
+        else:
+            print("Error: Missing lipstick_path argument.")
+            sys.exit(1)
 
-    opts = rnd_options(lipstick_path, iquest=iqu, modality='rt')
-    opts[answ] = True
-    shufOpts = shuffle_dic(opts)
-
-    MA = MultipleAnswer(lipstick, lipstick_path)
-    MA.load_question(qu)
-    MA.load_options(qu, answ, modality='rt')
-    MA.load_answers(shufOpts)
+    modality = 'rt'
+    
+    MA = MultipleAnswer(lipstick_path, modality=modality)
 
     perf = MA.run()
     print(perf)
+
+if __name__ == "__main__":
+    marte_main(sys.argv[1])
