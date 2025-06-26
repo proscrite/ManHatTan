f87268912c7c1ca5f34740efbea0325f4c6984f9
proscrite <pablogfr94@gmail.com>
2025-06-26 11:04:07 +0300

    Added passive conjugation, menukad/chaser filter, refactor functions and dicts for better readability

--- mht/scripts/python_scripts/pealim_scraper.py
+++ mht/scripts/python_scripts/pealim_scraper.py
@@ -5,30 +5,31 @@ from hebrew import Hebrew
 import random
 
 conjugation_key_map = {
-	"AP": "Present",
-	"PERF": "Past",
-	"IMPF": "Future",
-	"IMP": "Imperative",
-	"INF": "Infinitive",
-	"ms": "masculine singular",
-	"fs": "feminine singular",
-	"mp": "masculine plural",
-	"fp": "feminine plural",
-	"1s": "1st person singular",
-	"1p": "1st person plural",
-	"2ms": "2nd person masculine singular",
-	"2fs": "2nd person feminine singular",
-	"2mp": "2nd person masculine plural",
-	"2fp": "2nd person feminine plural",
-	"3ms": "3rd person masculine singular",
-	"3fs": "3rd person feminine singular",
-	"3mp": "3rd person masculine plural",
-	"3fp": "3rd person feminine plural",
-    "3p": "3rd person plural",
-	"L": "long form"
+    "AP": ("Present", "היום"),
+    "PERF": ("Past", "אתמול"),
+    "IMPF": ("Future", "מחר"),
+    "IMP": ("Imperative", ""),
+    "INF": ("Infinitive", "ל..."),
+    "ms": ("masculine singular", "הוא"),
+    "fs": ("feminine singular", "היא"),
+    "mp": ("masculine plural", "הם"),
+    "fp": ("feminine plural", "הן"),
+    "1s": ("1st person singular", "אני"),
+    "1p": ("1st person plural", "אנחנו"),
+    "2ms": ("2nd person masculine singular", "אתה"),
+    "2fs": ("2nd person feminine singular", "את"),
+    "2mp": ("2nd person masculine plural", "אתם"),
+    "2fp": ("2nd person feminine plural", "אתן"),
+    "3ms": ("3rd person masculine singular", "הוא"),
+    "3fs": ("3rd person feminine singular", "היא"),
+    "3mp": ("3rd person masculine plural", "הם"),
+    "3fp": ("3rd person feminine plural", "הן"),
+    "3p": ("3rd person plural", "הם/הן"),
+    "L": ("long form", " "),
+    "passive": ("passive", "(passive)"),
 }
 
-def get_verb_link(hebrew_verb):
+def scrap_verb_link(hebrew_verb):
     search_url = f"https://www.pealim.com/search/?q={hebrew_verb}"
     r = requests.get(search_url)
     soup = BeautifulSoup(r.text, 'html.parser')
@@ -49,7 +50,7 @@ def get_verb_link(hebrew_verb):
     first_url = verb_entries[0]['url'] if verb_entries else None
     return "https://www.pealim.com" + first_url if first_url else None
 
-def get_conjugation_dict(verb_url):
+def scrap_conjugation_dict(verb_url):
     if not verb_url:
         return {}
     r = requests.get(verb_url)
@@ -60,22 +61,39 @@ def get_conjugation_dict(verb_url):
     for entry in conjugation_entries:
         for div in entry.find_all('div', id=True):
             menukad = div.find('span', class_='menukad')
-            if menukad:
+            chaser = div.find('span', class_='chaser')
+            if chaser:
+                conjugated_verb_nikkud = chaser.get_text(strip=True)
+                verb_noniqqud = Hebrew(conjugated_verb_nikkud).no_niqqud()
+                verb_noniqqud = str(verb_noniqqud).replace('~', '').replace(' ', '')
+                conjug_dict[div['id']] = verb_noniqqud
+            elif menukad:
                 conjugated_verb_nikkud = menukad.get_text(strip=True)
                 verb_noniqqud = Hebrew(conjugated_verb_nikkud).no_niqqud()
+                verb_noniqqud = str(verb_noniqqud).replace('~', '').replace(' ', '')
                 conjug_dict[div['id']] = verb_noniqqud
     return conjug_dict
 
-def parse_conjugation_key(key, key_map):
-    """Split the key by dash and map each part using key_map if possible."""
+def parse_conjugation_key(key):
+    """
+    Split the key by dash and map each part using combined_conjugation_map.
+    Returns two strings: one for English, one for Hebrew hints.
+    """
     parts = key.split('-')
-    mapped_parts = [key_map.get(part, part) for part in parts]
-    return " - ".join(mapped_parts)
+    english_parts = []
+    hebrew_parts = []
+    for part in parts:
+        eng, heb = conjugation_key_map.get(part, (part, part))
+        english_parts.append(eng)
+        hebrew_parts.append(heb)
+    parsed_english = " - ".join(english_parts)
+    parsed_hebrew = " ,".join(hebrew_parts)
+    return parsed_english, parsed_hebrew
 
 def get_conj_dict(hebrew_verb):
     """Scrape conjugation data for a Hebrew verb from Pealim."""
-    verb_url = get_verb_link(hebrew_verb)
-    conjugation_dict = get_conjugation_dict(verb_url)
+    verb_url = scrap_verb_link(hebrew_verb)
+    conjugation_dict = scrap_conjugation_dict(verb_url)
 
     if not conjugation_dict:
         print(f"No conjugation data found for {hebrew_verb}")
@@ -89,23 +107,22 @@ def get_random_conjugation(hebrew_verb):
     """Get a random conjugation of a Hebrew verb."""
     conjugation_dict = get_conj_dict(hebrew_verb)
     if not conjugation_dict:
-        return None, None
+        return None, None, None
 
     # Randomly select a key from the conjugation dictionary
     random_key = random.choice(list(conjugation_dict.keys()))
     conjugated_verb = conjugation_dict[random_key]
     
     # Parse the key to get a human-readable form
-    parsed_key = parse_conjugation_key(random_key, conjugation_key_map)
-    
-    return parsed_key, conjugated_verb
+    parsed_english, parsed_hebrew = parse_conjugation_key(random_key)
+    return parsed_english, parsed_hebrew, conjugated_verb
 
 if __name__ == "__main__":
     hebrew_verb = sys.argv[1] if len(sys.argv) > 1 else "לכתוב"
-    parsed_key, conjugated_verb = get_random_conjugation(hebrew_verb)
-    if parsed_key and conjugated_verb:
+    parsed_english, parsed_hebrew, conjugated_verb = get_random_conjugation(hebrew_verb)
+    if parsed_english and conjugated_verb:
         print(f"Random conjugation for '{hebrew_verb}':")
-        print(f"Key: {parsed_key}")
+        print(f"Key: {parsed_english}")
         print(f"Conjugated Verb: {conjugated_verb}")
     else:
         print(f"No conjugation found for '{hebrew_verb}'.")
