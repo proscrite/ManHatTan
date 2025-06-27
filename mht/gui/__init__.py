1b47def856d04e7b8b4a6ce993d2f298d17dce3a
proscrite <pablogfr94@gmail.com>
2025-06-27 12:47:01 +0300

    Add configuration utility for Kivy logging and settings management, move imports to __init__.py

--- mht/gui/__init__.py
+++ mht/gui/__init__.py
@@ -1,1 +1,39 @@
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+from kivy.uix.dropdown import DropDown
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.button import Button
+from kivy.uix.image import Image
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.popup import Popup
+from kivy.clock import Clock
+from kivy.core.window import Window
+from kivy.logger import Logger as kvLogger
+from kivy.config import Config
+from kivy.app import App
+
+from mht.gui.screen_BaseExercise import BaseExerciseScreen
+from mht.gui.add_correctButton import CorrectionDialog
+from mht.gui.EachOption import EachOption
+from mht.gui.screen_writeInput import WriteInputScreen
+from mht.gui.screen_multipleAnswer import MultipleAnswerScreen
+from mht.gui.screen_team_manager import TeamScreen
+from mht.gui.screen_verbConjugation import ConjugationScreen
+
+
+__all__ = [
+    "ScreenManager", "Screen", "SlideTransition", "DropDown", "BoxLayout", "AnchorLayout",
+    "GridLayout", "Button", "Image", "Widget", "Label", "TextInput", "Popup",
+    "Clock", "Window", "kvLogger", "Config", "App",
+    "BaseExerciseScreen",
+    "CorrectionDialog",
+    "EachOption",
+    "WriteInputScreen",
+    "MultipleAnswerScreen",
+    "TeamScreen",
+    "ConjugationScreen",
+]
 
--- mht/gui/common.py
+++ mht/gui/common.py
@@ -29,6 +29,8 @@ matplotlib.use("Agg")
 
 # --- Constants ---
 ROOT_PATH = '/Users/pabloherrero/Documents/ManHatTan/mht'
+LIPSTICK_PATH = ROOT_PATH + '/data/processed/LIPSTICK/hebrew_db.lip'
+TEAM_LIP_PATH = LIPSTICK_PATH.replace('.lip', '_team.lip')
 FONT_HEB = ROOT_PATH + '/data/fonts/NotoSansHebrew.ttf'
 PATH_ANIM = ROOT_PATH + '/gui/Graphics/Battlers/'
 
