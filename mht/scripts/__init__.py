bf22a98de5c0c1877fa1ac4a7d5f6b15e2501aae
proscrite <pablogfr94@gmail.com>
2025-07-22 16:38:59 +0300

    Enhance Hebrew language support in table components; add font and alignment properties, and update data population methods to handle Hebrew text display.

--- mht/scripts/__init__.py
+++ mht/scripts/__init__.py
@@ -1,6 +1,6 @@
 from mht.scripts.python_scripts.krahtos import krahtos_main
 from mht.scripts.python_scripts.rashib import rashib_main
-from mht.scripts.python_scripts.gost import gost_main
+from mht.scripts.python_scripts.gost import gost_main, make_lang_dic
 from mht.scripts.python_scripts.bulkTranslate import bulkTranslate_main
 from mht.scripts.python_scripts.init_lipstick import init_lipstick_main
 from mht.scripts.python_scripts.update_lipstick import update_all
@@ -9,7 +9,7 @@ from mht.scripts.python_scripts.update_lipstick import update_all
 __all__ = [
     'krahtos_main',
     'rashib_main',
-    'gost_main',
+    'gost_main', 'make_lang_dic',
     'bulkTranslate_main',
     'init_lipstick_main',
     'update_all'
